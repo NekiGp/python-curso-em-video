d04a95f01f946b77e5d5eb89217670dcdda12e47
Nedson Roni <nedsonroni14@gmail.com>
2026-02-18 11:09:28 -0300

    Tratamento de dados e soma entre números
    Estudo de tipos primitivos (int) e operadores de soma.

---
+++ Mundo-1-Python/ex003.py
@@ -0,0 +1,6 @@
+#Exercício Python 3: Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1 = int(input('digite um numero'))
+n2 = int(input('digite outro numero'))
+s = n1 + n2
+print ('a soma entre {} e {} vale {}'.format(n1,n2,s))
