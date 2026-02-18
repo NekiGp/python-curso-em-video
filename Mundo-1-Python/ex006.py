facc1fa0afd62e90f4a52e692005d58e75ea2bd9
Nedson Roni <nedsonroni14@gmail.com>
2026-02-18 11:11:15 -0300

    Cálculo de Dobro, Triplo e Raiz Quadrada
    Implementação de cálculos matemáticos e exponenciação (**0.5).

---
+++ Mundo-1-Python/ex006.py
@@ -0,0 +1,6 @@
+#Exercício Python 006: Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input('digite um numero'))
+print ('O dobro de {} é igual a {}'.format(n, n*2))
+print ('O triplo de {} é igual a {}'.format(n, n*3))
+print ('A raiz quadrada de {} é igual a {}'.format(n, n**(1/2)))
